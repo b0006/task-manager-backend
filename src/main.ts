a51e2985fdc875f4507e2fdd4497e7e9bee21274
b0006 <qsefthuken@gmail.com>
2021-05-24 01:03:42 +0700

    fea(cors): allowed for localhost 3000

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
 
   // for class-transformer and class-validator
   app.useGlobalPipes(new ValidationPipe());
